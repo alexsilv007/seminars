9c6e95c9d3b59c82f0619ffd240ec9325d33d0b4
AlexSilv <silverprof@mail.ru>
2022-06-28 11:07:10 +0300

    seminar1

---
+++ seminar1/Program.cs
@@ -0,0 +1,86 @@
+﻿//Задача 1
+/*int num1, num2;
+
+Console.WriteLine("Input a num1: ");
+num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a num2: ");
+num2=Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+{
+    Console.WriteLine("max= " +num1);
+    Console.WriteLine("min= " +num2);
+}
+else
+if (num1<num2)
+{
+    Console.WriteLine("max= " +num2);
+    Console.WriteLine("min= " +num1);  
+}
+else
+if (num1==num2)
+{
+    Console.Write("Числа равны");
+}*/
+
+
+//Задача 2
+/*int num1, num2, num3;
+Console.WriteLine("Input a num1: ");
+num1=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a num2: ");
+num2=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input a num3: ");
+num3=Convert.ToInt32(Console.ReadLine());
+
+if (num1>num2)
+{
+    Console.WriteLine("max=" +num1);
+}
+else
+if (num2>num3)
+{
+    Console.WriteLine("max=" +num2);
+}
+else
+if (num3>num1)
+{
+   Console.WriteLine("max=" +num3); 
+}*/
+
+
+//Задача 3
+/*int num;
+
+Console.WriteLine("Input a num: ");
+num=Convert.ToInt32(Console.ReadLine());
+
+if ((num%2) ==0)
+{
+    Console.WriteLine("Чётное");
+}
+else
+{
+   Console.WriteLine("Нечётное"); 
+}*/
+
+
+//Задача 4
+/*int n;
+int x;
+
+Console.WriteLine("Input a n: ");
+n=Convert.ToInt32(Console.ReadLine());
+x=1;
+
+if(n<=1)
+{
+    Console.WriteLine("Impossible, minimum 1");
+}
+
+while (x<=n)
+{
+if (x%2==0)
+    Console.WriteLine(x + "");
+    x++;
+}*/
